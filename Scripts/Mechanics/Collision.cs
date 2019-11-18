3959003dda7aa58a67b1ddd1ffa512b0979fa055
Scytherhands <kevin.wang.2016@gmail.com>
2019-11-18 12:01:26 -0600

    initial commit Signed-off-by: Scytherhands <kevin.wang.2016@gmail.com>

---
+++ Scripts/Mechanics/Collision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Mechanics;
+
+public class Collision : PlayerController
+{
+    void OnCollisionEnter2D(Collision2D col) 
+    {
+        if(col.gameObject.name == "Floor") 
+        {
+            velocity.y = 7;
+        }
+    }
+}
